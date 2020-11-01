d428902df41710b87b48f0e5629f49156372594c
Jakub Rejmann <kubarejmann@wp.pl>
2020-11-01 17:14:52 +0100

    Animations improved

--- animations.js
+++ animations.js
@@ -1,72 +1,94 @@
-
 let t1 = gsap.timeline({defaults:{opacity:0}});
 
 t1.from('.hero__greybox', { y: 500, duration: .8})
 .from('.hero__img', {x:-1200,duration: 1.2})
-.from('.title-1', {duration: 1, y:-100}, "-=.5")
-.from('.title-2', {duration: 1, y:-100},"-=.5")
-.from('.title-3', {duration: 1},"-=.5")
+.from('.title-1', {duration: 1},"-=.6")
+.from('.title-2', {duration: 1},"-=.6")
+.from('.title-3', {duration: 1},"-=.6")
 
-gsap.from(".signature__title", {
-    scrollTrigger: ".signature__title", // start the animation when ".box" enters the viewport (once)
-    y: 100,
-    opacity: 0,
-    duration:.8
-  });
+const signatureContainer = document.querySelector('.signature');
 
-  gsap.from(".signature__paragraph", {
-    scrollTrigger: ".signature__paragraph", // start the animation when ".box" enters the viewport (once)
-    y: 100,
-    opacity: 0,
-    duration:.8,
-    delay:.4
-  });
-
-  gsap.from(".signature__sign", {
-    scrollTrigger: ".signature__sign", // start the animation when ".box" enters the viewport (once)
-    y: 100,
-    opacity: 0,
-    duration:.8,
-    delay:.8
-  });
-{
-  let p1 = gsap.timeline({
-    defaults:{opacity:0, duration:.8},
-    scrollTrigger:{
-  trigger: '.project1',
-  start:'center center'}
+gsap.from(signatureContainer.children, {
+  y: 100,
+  opacity: 0,
+  stagger: .2,
+  duration: 1,
+  scrollTrigger: {
+    trigger: signatureContainer,
+    start: '10% center'
+  }
 });
 
-p1.from('.project__img-1', {y:200})
-.from('.project__title-1',{x:-100} , "-=.5")
-.from('.project__description-1',{x:-100} , "-=.5")
-.from('.project__button-1',{}, "-=.5");
-}
 
-{
-let p2 = gsap.timeline({
-    defaults:{opacity:0, duration:.8},
-    scrollTrigger:{
-  trigger: '.project2',
-  start:'center center'}
+const tb1 = document.querySelector('.p1-textbox')
+
+gsap.from('.p1-img', {
+  y: 200,
+  opacity: 0,
+  duration: 1,
+  ease: 'easeInOut',
+  scrollTrigger: {
+    trigger: '.p1',
+    start: '30% center',
+  }
 });
 
-p2.from('.project__img-2', {y:-200})
-.from('.project__title-2',{x:100} , "-=.5")
-.from('.project__description-2',{x:100} , "-=.5")
-.from('.project__button-2',{}, "-=.5");
-}
+gsap.from(tb1.children, {
+  x: -100,
+  opacity: 0,
+  stagger: .2,
+  duration: 1,
+  scrollTrigger: {
+    trigger: '.p1',
+    start: '40% center',
+  }
+});
 
 
-{
-let p3 = gsap.timeline({
-    defaults:{opacity:0, duration:.8},
-    scrollTrigger:'.project3'});
+const tb2 = document.querySelector('.p2-textbox')
 
-p3.from('.project__img-3', {y:200})
-.from('.project__title-3',{x:-100} , "-=.5")
-.from('.project__description-3',{x:-100} , "-=.5")
-.from('.project__button-3',{}, "-=.5");
-}
+gsap.from('.p2-img', {
+  y: 200,
+  opacity: 0,
+  duration: 1,
+  ease: 'easeInOut',
+  scrollTrigger: {
+    trigger: '.p2',
+    start: '20% center',
+  }
+});
 
-  
+gsap.from(tb2.children, {
+  x: 100,
+  opacity: 0,
+  stagger: .2,
+  duration: 1,
+  scrollTrigger: {
+    trigger: '.p2',
+    start: '30% center',
+  }
+});
+
+const tb3 = document.querySelector('.p3-textbox')
+
+gsap.from('.p3-img', {
+  y: 200,
+  opacity: 0,
+  duration: 1,
+  ease: 'easeInOut',
+  scrollTrigger: {
+    trigger: '.p3',
+    start: '20% center',
+  }
+});
+
+gsap.from(tb3.children, {
+  x: -100,
+  opacity: 0,
+  stagger: .2,
+  duration: 1,
+  scrollTrigger: {
+    trigger: '.p3',
+    start: '30% center',
+  }
+});
