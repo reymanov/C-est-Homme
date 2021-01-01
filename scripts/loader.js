55ea00c08a8d9b7dee713fe724c63bf430994709
Jakub Rejmann <kubarejmann@wp.pl>
2021-01-01 16:48:28 +0100

    Loader improved

--- scripts/loader.js
+++ scripts/loader.js
@@ -1,12 +1,31 @@
 const heroIMG = document.querySelector(".hero__img");
-const loader = document.querySelector("#loader__container");
+const loaderContainer = document.querySelector("#loader__container");
+const loader = document.querySelector(".loader");
 
-if (heroIMG.complete) {
-  loaded();
-} else {
-  heroIMG.addEventListener("load", loaded);
-}
+setTimeout(() => {
+  if (heroIMG.complete) {
+    loaded();
+  } else {
+    heroIMG.addEventListener("load", loaded);
+  }
+}, 2000);
 
 function loaded() {
-  loader.style.display = "none";
+  loader.style.opacity = "0";
+  setTimeout(() => {
+    loaderContainer.style.opacity = "0";
+
+    const t1 = gsap.timeline({ defaults: { opacity: 0 } });
+
+    t1.from(".hero__greybox", { y: 500, duration: 0.8 })
+      .from(".hero__img", { x: -1200, duration: 1.2 })
+      .from(".title-1", { duration: 1 }, "-=.6")
+      .from(".title-2", { duration: 1 }, "-=.6")
+      .from(".title-3", { duration: 1 }, "-=.6");
+  }, 500);
+}
+
+if ((loaderContainer.style.opacity = "100")) {
+  document.documentElement.style.overflow = "hidden";
+  document.body.scroll = "no";
 }
